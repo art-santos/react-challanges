13b26ab87163623e4fa40d7a6ea49d2ec7446eeb
Arthur Santos <arthursantosdeveloper@gmail.com>
2024-06-23 19:20:54 -0300

    chore: virtual piano initial code

--- virtual-piano/src/App.js
+++ virtual-piano/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react'
+import ReactDOM from 'react-dom/client';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const notes = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
+
+const PianoKey = ({ note, isPressed, onPress }) => (
+
+  {/*
+  TODO: CREATE VISUALS FOR THE KEYS AS BUTTONS
+  TODO: GIVE PRESSED BUTTONS THE pressed CLASSNAME
+  TODO: ADD FUNCTIONALITY TO BUTTONS & RENDER THEIR NOTE NAME
+  */}
+);
+
+const Piano = ({ pressedNotes, onPressKey }) => (
+  <div>
+    keys go here
+  {/*TODO: RENDER ALL KEYS HERE*/}
+  </div>
+);
+
+const App = () => {
+  const [pressedNotes, setPressedNotes] = useState([]);
+
+  const onPressKey = note => {
+    setPressedNotes(prevPressedNotes =>
+      prevPressedNotes.includes(note)
+        ? prevPressedNotes.filter(n => n !== note)
+        : [...prevPressedNotes, note]
+    );
+  };
+
+  return <Piano pressedNotes={pressedNotes} onPressKey={onPressKey} />;
+};
 
 export default App;
