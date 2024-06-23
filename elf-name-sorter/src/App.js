583d5681bd220ad048c963b2a52028be6236c32c
Arthur Santos <arthursantosdeveloper@gmail.com>
2024-06-23 19:14:11 -0300

    chore: code for elf map

--- elf-name-sorter/src/App.js
+++ elf-name-sorter/src/App.js
@@ -1,25 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+import React,{useState, useEffect} from 'react'
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+// They don't necessarily need to take props
+// This one also has an explicit return
+const App = () => {
+  const [elves, setElves] = useState([
+    'Buddy',
+    'Dobby',
+    'Winky',
+    'Bing',
+    'Bernard'
+  ]);
+  
+  const [sort, setSort] = useState([""])
+  
+  useEffect(() => {
+    const sorted = [...elves].sort()
+    setSort(sorted)
+  }, [])
+
+  const stringfiedElves = elves
+  const stringfiedSort = sort
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className="Card">
+      {stringfiedElves.map((elf, index) => (
+        <div key={index}>{elf}</div>
+      ))}
+      </div>
+      <div className="Card">
+      {stringfiedSort.map((elf, index) => (
+        <div key={index}>{elf}</div>
+      ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
