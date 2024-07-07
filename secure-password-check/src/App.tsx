bead02878e7a04d350c87df3e0cbf754138a03b7
Arthur Santos <arthursantosdeveloper@gmail.com>
2024-07-07 20:02:59 -0300

    chore: Add password input fields and validation functions

--- secure-password-check/src/App.tsx
+++ secure-password-check/src/App.tsx
@@ -2,25 +2,29 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // TODO: Add state variables for password and confirm password
+// TODO: Add event handlers for updating password and confirm password state
+
+function checkPasswordsMatch() {
+  // TODO: Compare password and confirm password state and return true if they match, false if they don't
+}
+
+function checkPasswordStrength() {
+  // TODO: Use a regular expression to check if password includes at least one special character
+  // TODO: Check if password is at least 8 characters long
+  // TODO: Return true if password has a special character and is at least 8 characters long, false otherwise
+}
+
+return (
+  <form>
+    {/* TODO: Add password input field */}
+    {/* TODO: Add confirm password input field */}
+    {/* TODO: Add message to display whether passwords match */}
+    {/* TODO: Add message to display whether password is strong */}
+  </form>
+);
 }
 
 export default App;
